82baba9531e3f54bab6b54c36bdcce996f7607cc
Devon Kiss <tuffs@gmx.com>
2018-06-30 23:15:10 -0500

    Added jest babel-preset-env and react-test-renderer to application.

---
+++ jest/__tests__/App.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import App from '../../app/src/containers/App';
+import renderer from 'react-test-renderer';
+
+test('Example Test Placeholder', () => {
+	// Example test file...
+});
