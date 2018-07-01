a93d2247740c5e4f17cb2a94ee847ed85a84eabf
Devon Kiss <tuffs@gmx.com>
2018-06-30 21:33:41 -0500

    Removed components.

--- app/src/containers/App.jsx
+++ app/src/containers/App.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import Logo from './../components/Logo.jsx'; //Component with props but no state
-import Counter from './../components/Counter.jsx';
 
 export default class App extends Component {
     render() {
         return (
           <div className="app-container">
-            <Logo />
-            <Counter />             
+            <h1>Base App</h1>
           </div>
         );
     }
